5e81129890c8d5dd62f65d28485eec339a526a62
Muncherxyz <101988752+Muncherxyz@users.noreply.github.com>
2024-10-10 05:44:24 -0500

    --Enemy Instance

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject zombie;
+
+    public void StartGame()
+    {
+    
+    }
+}
